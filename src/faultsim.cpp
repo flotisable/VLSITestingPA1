a884de92ef89a9dee2000904ee358b38c31beb04
flotisable <s09930698@gmail.com>
2018-10-04 12:35:32 +0800

    add some comments

--- src/faultsim.cpp
+++ src/faultsim.cpp
@@ -207,17 +207,17 @@ void ATPG::fault_sim_a_vector(const string& vec, int& num_of_current_detect) {
         /*TODO*/
         //Hint:Use mask to get the value of faulty wire and check every fault in packet
     //---------------------------------------- hole ---------------------------------------------
-        if( w->flag & OUTPUT )
+        if( w->flag & OUTPUT ) // check if the wire is PO
         {
           for( int i = 0 ; i < num_of_pattern ; ++i )
           {
-             if( !( ( w->wire_value1 ^ Unknown[i]     ) & Mask[i] ) ) continue;
-             if( !( ( w->wire_value1 ^ w->wire_value2 ) & Mask[i] ) ) continue;
+             if( !( ( w->wire_value1 ^ Unknown[i]     ) & Mask[i] ) ) continue; // check if the value is unknown
+             if( !( ( w->wire_value1 ^ w->wire_value2 ) & Mask[i] ) ) continue; // check if the value if different
 
              simulated_fault_list[i]->detect = TRUE;
           }
         }
-        w->wire_value2 = w->wire_value1;
+        w->wire_value2 = w->wire_value1; // reset the faulty value to fault-free value
     //-------------------------------------------------------------------------------------------
         /*TODO*/
 	    } // pop out all faulty wires
